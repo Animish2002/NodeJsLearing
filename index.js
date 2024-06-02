abba5a3b18fb04eb25de900f9bbfc1562379a77e
Animish2002 <animishchopade123@gmail.com>
2024-06-02 23:17:22 +0530

    http method implemented get, post , get all

--- index.js
+++ index.js
@@ -1,26 +1,57 @@
 const express = require("express");
+const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 
 const app = express();
+const PORT = 8080;
+
+//middleware  => yeh jo form data ata hai usse body mai add krne ka kaam karta hai
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.end("Hello HomePage!");
+  res.end("Home Page");
 });
 
-app.get("/about", (req, res) => {
-  const { name, lang, feeling } = req.query;
-  if (name && lang && feeling) {
-    res.send(
-      `Hello, this is ${name} learning ${lang} right now and I ${feeling} it`
-    );
-  } else {
-    res.send("Hello About page");
-  }
+app.get("/users", (req, res) => {
+  //this  is server side rendering
+  const html = `
+  <ul>
+  ${users.map((user) => `<li>${user.first_name}</li> `).join("")}
+  </ul>`;
+  res.send(html);
 });
 
-app.get("/contact", (req, res) => {
-  res.end("Hello Contact Page!");
+app.get("/api/users", (req, res) => {
+  //GET ALL USERS
+  return res.json(users); // return all the users in json format
 });
 
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
+//for same route we can use get,put,delete
+app
+  .route("/api/user/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    //it is used to update user
+    return res.json({ status: "Pending" });
+  })
+  .delete((req, res) => {
+    //it is used to delete user
+    return res.json({ status: "Pending" });
+  });
+
+app.post("/api/user", (req, res) => {
+  //it is used to create new user
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "Pending", id: users.length });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on Port number ${PORT}`);
 });
